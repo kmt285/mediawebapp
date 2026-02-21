769f5e8277401db0128da0bb699048be584841e2
kmt285 <248855908+kmt285@users.noreply.github.com>
2026-02-21 14:55:50 +0630

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "supersecret")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 3000
 MAX_FILE_SIZE = 1100 * 1024 * 1024
-mongo_client = AsyncIOMotorClient(MONGO_URL, maxPoolSize=1100) 
+mongo_client = AsyncIOMotorClient(MONGO_URL, maxPoolSize=50) 
 db = mongo_client["fileshare_db"]
 
 # --- Setup ---
@@ -55,7 +55,6 @@ oauth.register(
 )
 
 # Database
-mongo_client = AsyncIOMotorClient(MONGO_URL)
 db = mongo_client["fileshare_db"]
 files_collection = db["files"]
 folders_collection = db["folders"]
